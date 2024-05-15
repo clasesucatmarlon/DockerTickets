a84090817650cd570d6ed4dabe9fe3737b128fdd
clasesucatmarlon <clasesucatmarlon@gmail.com>
2024-05-15 14:23:50 -0500

    feat(AppTickets): :sparkles: Arreglado modelo para listar todos los tickets
    Arreglado modelo para listar todos los tickets

--- server/src/controllers/tickets.controller.js
+++ server/src/controllers/tickets.controller.js
@@ -67,35 +67,7 @@ export class TicketController {
     // OBTENER TODOS LOS TICKETS
     static getAllTickets = async (req, res) => {
         try {
-            const { user } = req;
-            const { page } = req.body;
-
-            const limit = 5;
-
-            let tickets = [];
-
-            if (!user.permissions.includes("support")) {
-                tickets = await Ticket.find({ createdBy: user._id })
-                    .populate({
-                        path: "createdBy",
-                        model: "User",
-                        select: "-password -__v -updatedAt -isActive -isConfirmed",
-                    })
-                    .sort({ createdAt: "desc" })
-                    .skip((page - 1) * limit)
-                    .limit(limit);
-            } else {
-                tickets = await Ticket.find()
-                    .populate({
-                        path: "createdBy",
-                        model: "User",
-                        select: "-password -__v -updatedAt -isActive -isConfirmed",
-                    })
-                    .sort({ createdAt: "desc" })
-                    .skip((page - 1) * limit)
-                    .limit(limit);
-            }
-
+            const tickets = await Ticket.find();
             res.status(200).json(tickets);
         } catch (error) {
             console.log(error);
